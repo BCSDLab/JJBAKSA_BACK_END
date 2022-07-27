f5e3e9b08031805f174510f03d26c967ae448c88
YunSeojin <tkaltk123@gmail.com>
2022-07-28 01:56:00 +0900

    프로젝트 기본 설정

---
+++ src/main/java/com/jjbacsa/jjbacsabackend/JjbacsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jjbacsa.jjbacsabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjbacsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjbacsaBackendApplication.class, args);
+	}
+
+}
