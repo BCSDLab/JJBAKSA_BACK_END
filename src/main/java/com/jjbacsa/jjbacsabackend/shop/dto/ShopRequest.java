bef67056a782cb8c5295b57c3f4ffa9383c87d30
YunSeojin <tkaltk123@gmail.com>
2022-07-28 23:31:08 +0900

    feat: [jp-000] shop entity, dto, repo 추가

---
+++ src/main/java/com/jjbacsa/jjbacsabackend/shop/dto/ShopRequest.java
@@ -0,0 +1,12 @@
+package com.jjbacsa.jjbacsabackend.shop.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShopRequest {
+
+}
---
+++ src/main/java/com/jjbacsa/jjbacsabackend/shop/repository/ShopRepository.java
@@ -0,0 +1,15 @@
+package com.jjbacsa.jjbacsabackend.shop.repository;
+
+import com.jjbacsa.jjbacsabackend.shop.entity.ShopEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ShopRepository extends JpaRepository<ShopEntity, Long> {
+
+    Optional<ShopEntity> findByPlaceId(String placeId);
+
+    boolean existsByPlaceId(String placeId);
+}
