e19384993d5445781e28bd6b0f0d3cc496ab5485
YunSeojin <tkaltk123@gmail.com>
2022-07-31 19:51:17 +0900

    fix: create 쿼리 수정, mapstruct 예시 코드 추가
    - 이모지 지원 문제로 utf8mb4 적용
    - int 길이 지원 종료로  TINYINT(1) -> TINYINT 변경

--- src/main/resources/db/migration/V2__create.sql
+++ src/main/resources/db/migration/V2__create.sql
@@ -1,20 +1,19 @@
-
 CREATE TABLE IF NOT EXISTS image
 (
-    id         BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted TINYINT(1) NOT NULL DEFAULT 0,
-    path       TEXT       NOT NULL
-) default character set utf8
-  collate utf8_general_ci;
+    id         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted TINYINT   NOT NULL DEFAULT 0,
+    path       TEXT      NOT NULL
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS user
 (
     id               BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     created_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at       TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted       TINYINT(1)   NOT NULL DEFAULT 0,
+    is_deleted       TINYINT      NOT NULL DEFAULT 0,
     account          VARCHAR(255) NOT NULL,
     password         VARCHAR(255) NOT NULL,
     email            VARCHAR(255) NOT NULL,
@@ -23,8 +22,8 @@ CREATE TABLE IF NOT EXISTS user
     oauth_type       VARCHAR(255) NOT NULL,
     user_type        VARCHAR(255) NOT NULL,
     FOREIGN KEY (profile_image_id) REFERENCES image (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS user_count
 (
@@ -32,22 +31,22 @@ CREATE TABLE IF NOT EXISTS user_count
     review_count INT    NOT NULL DEFAULT 0,
     friend_count INT    NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS shop
 (
     id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted    TINYINT(1)   NOT NULL DEFAULT 0,
+    is_deleted    TINYINT      NOT NULL DEFAULT 0,
     place_id      VARCHAR(255) NOT NULL,
     place_name    VARCHAR(255) NOT NULL,
     x             VARCHAR(255) NOT NULL,
     y             VARCHAR(255) NOT NULL,
     category_name VARCHAR(255) NOT NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS shop_count
 (
@@ -55,110 +54,110 @@ CREATE TABLE IF NOT EXISTS shop_count
     total_rating INT    NOT NULL DEFAULT 0,
     rating_count INT    NOT NULL DEFAULT 0,
     FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE CASCADE
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS rating
 (
-    id           BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted   TINYINT(1) NOT NULL DEFAULT 0,
+    id           BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted   TINYINT   NOT NULL DEFAULT 0,
     user_id      BIGINT,
     shop_id      BIGINT,
-    rating_score INT        NOT NULL,
+    rating_score INT       NOT NULL,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL,
     FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS review
 (
-    id         BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted TINYINT(1) NOT NULL DEFAULT 0,
+    id         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted TINYINT   NOT NULL DEFAULT 0,
     writer_id  BIGINT,
     shop_id    BIGINT,
     content    TEXT,
-    is_temp    TINYINT(1) NOT NULL DEFAULT 0,
+    is_temp    TINYINT   NOT NULL DEFAULT 0,
     FOREIGN KEY (writer_id) REFERENCES user (id) ON DELETE SET NULL,
     FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS review_image
 (
-    id         BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted TINYINT(1) NOT NULL DEFAULT 0,
+    id         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted TINYINT   NOT NULL DEFAULT 0,
     review_id  BIGINT,
     image_id   BIGINT,
     FOREIGN KEY (review_id) REFERENCES review (id) ON DELETE SET NULL,
     FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS follow
 (
-    id          BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted  TINYINT(1) NOT NULL DEFAULT 0,
+    id          BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted  TINYINT   NOT NULL DEFAULT 0,
     user_id     BIGINT,
     follower_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL,
     FOREIGN KEY (follower_id) REFERENCES user (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS follow_request
 (
-    id          BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at  TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted  TINYINT(1) NOT NULL DEFAULT 0,
+    id          BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted  TINYINT   NOT NULL DEFAULT 0,
     user_id     BIGINT,
     follower_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL,
     FOREIGN KEY (follower_id) REFERENCES user (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS scrap_directory
 (
     id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted TINYINT(1)   NOT NULL DEFAULT 0,
+    is_deleted TINYINT      NOT NULL DEFAULT 0,
     name       VARCHAR(255) NOT NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS scrap
 (
-    id           BIGINT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    created_at   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-    updated_at   TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted   TINYINT(1) NOT NULL DEFAULT 0,
+    id           BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+    updated_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
+    is_deleted   TINYINT   NOT NULL DEFAULT 0,
     user_id      BIGINT,
     shop_id      BIGINT,
     directory_id BIGINT,
     FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL,
     FOREIGN KEY (shop_id) REFERENCES shop (id) ON DELETE SET NULL,
     FOREIGN KEY (directory_id) REFERENCES scrap_directory (id) ON DELETE SET NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS post
 (
     id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
-    is_deleted TINYINT(1)   NOT NULL DEFAULT 0,
+    is_deleted TINYINT      NOT NULL DEFAULT 0,
     title      VARCHAR(255) NOT NULL,
     content    TEXT         NOT NULL,
     board_type VARCHAR(255) NOT NULL
-) default character set utf8
-  collate utf8_general_ci;
+) default character set utf8mb4
+  collate utf8mb4_general_ci;
