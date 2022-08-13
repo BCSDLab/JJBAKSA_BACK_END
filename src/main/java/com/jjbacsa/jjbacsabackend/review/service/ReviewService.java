3aa7ccf499df68b930de6f811f7852efe782a9ac
YeJunLim <dpwns523@naver.com>
2022-08-13 21:10:55 +0900

    feat: [jb-006] 리뷰 서비스 추가
    리뷰 service에 대한 인터페이스와 구현체를 추가하였다

---
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/service/ReviewService.java
@@ -0,0 +1,18 @@
+package com.jjbacsa.jjbacsabackend.review.service;
+
+import com.jjbacsa.jjbacsabackend.review.dto.ReviewDto;
+import com.jjbacsa.jjbacsabackend.review.dto.ReviewWithImageDto;
+import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewResponse;
+import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewWithImageResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ReviewService {
+
+    ReviewWithImageResponse createReview(ReviewWithImageDto reviewWithImageDto);
+    void modifyReview(ReviewWithImageDto reviewWithImageDto);
+    void deleteReview(Long reviewId);
+    ReviewWithImageResponse getReview(Long reviewId);
+    Page<ReviewResponse> searchShopReviews(Long shopId, Pageable pageable);
+    Page<ReviewResponse> searchWriterReviews(Long writerId, Pageable pageable);
+}
---
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/serviceImpl/ReviewServiceImpl.java
@@ -0,0 +1,69 @@
+package com.jjbacsa.jjbacsabackend.review.serviceImpl;
+
+import com.jjbacsa.jjbacsabackend.review.dto.ReviewDto;
+import com.jjbacsa.jjbacsabackend.review.dto.ReviewWithImageDto;
+import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewResponse;
+import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewWithImageResponse;
+import com.jjbacsa.jjbacsabackend.review.entity.ReviewEntity;
+import com.jjbacsa.jjbacsabackend.review.mapper.ReviewMapper;
+import com.jjbacsa.jjbacsabackend.review.repository.ReviewRepository;
+import com.jjbacsa.jjbacsabackend.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public ReviewWithImageResponse createReview(ReviewWithImageDto reviewWithImageDto) {
+        ReviewEntity review = reviewRepository.save(ReviewMapper.INSTANCE.toReviewEntity(reviewWithImageDto));
+        return ReviewMapper.INSTANCE.fromReviewEntityWithImages(review);
+    }
+
+    @Override
+    public void modifyReview(ReviewWithImageDto reviewWithImageDto) {
+        try {
+            ReviewEntity review = reviewRepository.getById(reviewWithImageDto.getId());
+            if (reviewWithImageDto.getContent() != null) review.setContent(reviewWithImageDto.getContent());  // not null 컬럼
+            // save하지 않아도 트랜잭션안에서 변경감지로 처리됨.
+        }catch (EntityNotFoundException e){
+            log.warn("리뷰 업데이트 실패, 리뷰를 찾을 수 없습니다 - dto: {}", reviewWithImageDto);
+        }
+    }
+
+    @Override
+    public void deleteReview(Long reviewId) {
+        reviewRepository.deleteById(reviewId);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ReviewWithImageResponse getReview(Long reviewId) {
+        return reviewRepository.findById(reviewId)
+                .map(ReviewMapper.INSTANCE::fromReviewEntityToReviewWithImages)
+                .orElseThrow(() -> new EntityNotFoundException("리뷰가 없습니다:" + reviewId));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ReviewResponse> searchShopReviews(Long shopId, Pageable pageable) {
+        return reviewRepository.findAllByShopId(shopId, pageable).map(ReviewMapper.INSTANCE::fromReviewEntity);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<ReviewResponse> searchWriterReviews(Long writerId, Pageable pageable){
+        return reviewRepository.findAllByWriterId(writerId, pageable).map(ReviewMapper.INSTANCE::fromReviewEntity);
+    }
+}
