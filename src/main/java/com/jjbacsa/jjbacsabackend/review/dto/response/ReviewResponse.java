2564e329af53b322e4ec259602c9ed61198f470c
YeJunLim <dpwns523@naver.com>
2022-08-13 21:04:16 +0900

    feat: [jb-006] 리뷰 response 추가
    리뷰 단건 조회와 리스트 조회에 따라 이미지 포함여부가 나뉘기 때문에 두 개의 response를 추가하였음

---
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/dto/response/ReviewResponse.java
@@ -0,0 +1,19 @@
+package com.jjbacsa.jjbacsabackend.review.dto.response;
+
+import com.jjbacsa.jjbacsabackend.shop.dto.ShopDto;
+import com.jjbacsa.jjbacsabackend.user.dto.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ReviewResponse {
+    private Long id;
+    private UserDto userDto;
+    private ShopDto shopDto;
+    private String content;
+    private int isTemp;
+    private LocalDateTime createdAt;
+}
