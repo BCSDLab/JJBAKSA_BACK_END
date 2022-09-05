90d195cd55d80dd75550f9d6e1ddca2c53aba4c3
YeJunLim <dpwns523@naver.com>
2022-09-06 02:02:04 +0900

    fix: [jb-006] ModelAttribute 어노테이션을 활용해 이미지 파일을 받는 방식으로 변경
    이전에 form-data형식을 multipartfile와 dto를 분리해서 두 개의 content-type으로 데이터를 받았지만, swagger에서는 content-type을 분리하지 못해 postman으로만 테스트해야 했음. swagger 공식사이트를 확인해보니, 하나의 content-type으로 입력받는것을 권장하여 변경하였음.
    이에 따라 Controller 파라미터와 Dto도 수정함.
    
    * https://springdoc.org/#how-are-endpoints-with-multiple-consuming-media-types-supported

--- src/main/java/com/jjbacsa/jjbacsabackend/review/controller/ReviewController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/controller/ReviewController.java
@@ -2,9 +2,7 @@ package com.jjbacsa.jjbacsabackend.review.controller;
 
 import com.jjbacsa.jjbacsabackend.etc.annotations.Auth;
 import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewModifyRequest;
-import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewModifyRequestDto;
 import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewRequest;
-import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewRequestDto;
 import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewDeleteResponse;
 import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewResponse;
 import com.jjbacsa.jjbacsabackend.review.service.ReviewService;
@@ -16,11 +14,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -30,9 +28,8 @@ public class ReviewController {
 
     @Auth
     @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
-    @PostMapping(value = "/review")
-    public ResponseEntity<ReviewResponse> createReview(@RequestPart ReviewRequestDto reviewRequestDto, @RequestPart(required = false) List<MultipartFile> images) throws Exception {
-        ReviewRequest reviewRequest = ReviewRequest.createReviewRequest(images, reviewRequestDto);
+    @PostMapping(value = "/review", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ReviewResponse> createReview(@ModelAttribute ReviewRequest reviewRequest) throws Exception {
         return new ResponseEntity<>(reviewService.createReview(reviewRequest), HttpStatus.CREATED);
     }
     @Auth
@@ -47,8 +44,6 @@ public class ReviewController {
         return new ResponseEntity<>(reviewService.searchShopReviews(shopId, pageable), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
     @GetMapping(value = "/review/search/writer")
     public ResponseEntity<Page<ReviewResponse>>searchWriterReview(@RequestParam("id") Long writerId, @PageableDefault(size = 3, sort = "createdAt", direction = Sort.Direction.DESC)Pageable pageable){
         return new ResponseEntity<>(reviewService.searchWriterReviews(writerId, pageable), HttpStatus.OK);
@@ -62,9 +57,8 @@ public class ReviewController {
     }
     @Auth
     @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
-    @PatchMapping(value="/review")
-    public ResponseEntity<ReviewResponse> modifyReview(@RequestPart ReviewModifyRequestDto reviewModifyRequestDto, @RequestPart(required = false) List<MultipartFile> images) throws Exception {
-        ReviewModifyRequest reviewModifyRequest = ReviewModifyRequest.createReviewModifyRequest(images, reviewModifyRequestDto);
+    @PatchMapping(value="/review", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ReviewResponse> modifyReview(@ModelAttribute @RequestBody ReviewModifyRequest reviewModifyRequest) throws Exception {
         return new ResponseEntity<>(reviewService.modifyReview(reviewModifyRequest), HttpStatus.OK);
     }
 
