94cd99e616f811510fcebae48ad9697a70a756db
LeeInsuk <inkcloud365@gmail.com>
2022-09-16 23:46:14 +0900

    fix : Delete Unused Package Path
    - ReviewService에서 사용하지 않는 서비스 패키지 참조 제거

--- src/main/java/com/jjbacsa/jjbacsabackend/review/service/ReviewService.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/service/ReviewService.java
@@ -1,14 +1,11 @@
 package com.jjbacsa.jjbacsabackend.review.service;
 
 import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewModifyRequest;
-import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewModifyRequestDto;
 import com.jjbacsa.jjbacsabackend.review.dto.request.ReviewRequest;
 import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewDeleteResponse;
 import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.io.IOException;
 
 public interface ReviewService {
 
