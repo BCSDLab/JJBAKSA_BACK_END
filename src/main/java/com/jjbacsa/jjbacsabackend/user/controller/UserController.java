6d9a5dbecc246e1c56ae2ed22b19c5c6a186bd78
LeeInsuk <inkcloud365@gmail.com>
2022-09-18 01:44:38 +0900

    docs : Add Swagger API Document
    - User 도메인 API의 Swagger를 통해 생성되는 문서 수정

--- src/main/java/com/jjbacsa/jjbacsabackend/user/controller/UserController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/user/controller/UserController.java
@@ -4,8 +4,7 @@ import com.jjbacsa.jjbacsabackend.etc.dto.Token;
 import com.jjbacsa.jjbacsabackend.user.dto.UserRequest;
 import com.jjbacsa.jjbacsabackend.user.dto.UserResponse;
 import com.jjbacsa.jjbacsabackend.user.service.UserService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.Authorization;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,30 +18,48 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController {
     private final UserService userService;
 
+    @ApiOperation(
+            value = "회원가입",
+            notes = "회원가입을 진행합니다.\n" +
+                    "필요한 필드 : account, password, email")
     @PostMapping(value = "/user")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) throws Exception {
         return new ResponseEntity<>(userService.register(request), HttpStatus.CREATED);
     }
 
+    @ApiOperation(
+            value = "로그인",
+            notes = "로그인을 진행합니다.\n" +
+                    "필요한필드 : account, password")
     @PostMapping(value = "/user/login")
     public ResponseEntity<Token> login(@RequestBody UserRequest request, HttpServletResponse httpResponse) throws Exception{
         return new ResponseEntity<>(userService.login(request), HttpStatus.OK);
     }
 
-    //@Auth
+    @ApiOperation(
+            value = "본인 정보 확인",
+            notes = "로그인 유저 정보 확인(타 기능용 메소드입니다. 작성시 유의바랍니다.)",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
     @PreAuthorize("hasRole('NORMAL')")
-    @ApiOperation( value = "",notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
     @GetMapping(value = "/user/me")
     public ResponseEntity<UserResponse> getMe() throws Exception{
         return new ResponseEntity<>(userService.getLoginUser(), HttpStatus.OK);
     }
 
+    @ApiOperation(
+            value = "로그아웃",
+            notes = "로그아웃합니다.",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
     @DeleteMapping(value = "/user/logout")
-    public ResponseEntity<String> logout()throws Exception{
+    public ResponseEntity<String> logout() throws Exception{
         userService.logout();
         return new ResponseEntity<>("success", HttpStatus.NO_CONTENT);
     }
 
+    @ApiOperation(
+            value = "토큰 재발급",
+            notes = "accessToken과 refreshToken을 재발급합니다.",
+            authorizations = @Authorization(value = "Bearer + refreshToken"))
     @GetMapping("/user/refresh")
     public ResponseEntity<Token> refresh() throws Exception{
         return new ResponseEntity<>(userService.refresh(), HttpStatus.OK);
