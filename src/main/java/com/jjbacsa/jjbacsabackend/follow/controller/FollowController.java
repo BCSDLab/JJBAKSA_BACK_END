15a0c4e683a2779c74688dfb5e92afb665678cd9
YunSeojin <tkaltk123@gmail.com>
2022-10-02 13:58:26 +0900

    docs: follow swagger 설명 추가

--- src/main/java/com/jjbacsa/jjbacsabackend/follow/controller/FollowController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/follow/controller/FollowController.java
@@ -1,12 +1,12 @@
 package com.jjbacsa.jjbacsabackend.follow.controller;
 
-import com.jjbacsa.jjbacsabackend.etc.annotations.Auth;
 import com.jjbacsa.jjbacsabackend.follow.dto.FollowRequest;
 import com.jjbacsa.jjbacsabackend.follow.dto.FollowRequestResponse;
 import com.jjbacsa.jjbacsabackend.follow.dto.FollowResponse;
 import com.jjbacsa.jjbacsabackend.follow.service.FollowService;
 import com.jjbacsa.jjbacsabackend.user.dto.UserResponse;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -14,16 +14,23 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+//Todo: Pageable을 service에서 생성하기
+//Todo: cursor를 String으로 받지 않고 parameter로 받아서 생성하기
 public class FollowController {
 
     private final FollowService service;
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로우 요청",
+            notes = "중복으로 요청할 수 없음\n\n" +
+                    "상대가 먼저 요청했을 경우 요청할 수 없음",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @PostMapping(value = "/follow/requests")
     public ResponseEntity<FollowRequestResponse> requestFollow(
             @RequestBody FollowRequest request) throws Exception {
@@ -31,8 +38,11 @@ public class FollowController {
         return new ResponseEntity<>(service.request(request), HttpStatus.CREATED);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "보낸 팔로우 요청 조회",
+            notes = "오프셋 기반 페이징(페이지 번호로 조회)",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/follow/requests/send")
     public ResponseEntity<Page<FollowRequestResponse>> getSendRequests(
             @PageableDefault(size = 20) Pageable pageable) throws Exception {
@@ -40,8 +50,11 @@ public class FollowController {
         return new ResponseEntity<>(service.getSendRequests(pageable), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "받은 팔로우 요청 조회",
+            notes = "오프셋 기반 페이징(페이지 번호로 조회)",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/follow/requests/receive")
     public ResponseEntity<Page<FollowRequestResponse>> getReceiveRequests(
             @PageableDefault(size = 20) Pageable pageable) throws Exception {
@@ -49,39 +62,48 @@ public class FollowController {
         return new ResponseEntity<>(service.getReceiveRequests(pageable), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로우 요청 취소",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @DeleteMapping(value = "/follow/requests/{request_id}/cancel")
     public ResponseEntity<Void> cancelFollow(
-            @PathVariable("request_id") Long requestId) throws Exception {
+            @ApiParam(value = "팔로우 요청 ID") @PathVariable("request_id") Long requestId) throws Exception {
 
         service.cancel(requestId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로우 요청 거절",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @DeleteMapping(value = "/follow/requests/{request_id}/reject")
     public ResponseEntity<Void> rejectFollow(
-            @PathVariable("request_id") Long requestId) throws Exception {
+            @ApiParam(value = "팔로우 요청 ID") @PathVariable("request_id") Long requestId) throws Exception {
 
         service.reject(requestId);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로우 요청 수락",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @PostMapping(value = "/follow/requests/{request_id}/accept")
     public ResponseEntity<FollowResponse> acceptFollow(
-            @PathVariable("request_id") Long requestId) throws Exception {
+            @ApiParam(value = "팔로우 요청 ID") @PathVariable("request_id") Long requestId) throws Exception {
 
         return new ResponseEntity<>(service.accept(requestId), HttpStatus.CREATED);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로워 목록 조회",
+            notes = "커서 기반 페이징(마지막 객체로 조회)",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/follow/followers")
     public ResponseEntity<Page<UserResponse>> getFollowers(
             @RequestParam(required = false) String cursor,
@@ -90,14 +112,16 @@ public class FollowController {
         return new ResponseEntity<>(service.getFollowers(cursor, pageable), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로워 삭제",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @DeleteMapping(value = "/follow/followers")
     public ResponseEntity<Void> deleteFollow(
             @RequestBody FollowRequest request) throws Exception {
 
         service.delete(request);
 
-        return new ResponseEntity<>(HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
