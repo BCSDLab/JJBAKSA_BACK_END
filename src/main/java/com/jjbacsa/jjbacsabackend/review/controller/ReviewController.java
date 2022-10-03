be97ba7c45eef20b2b47d61b25760ad5531219a8
YeJunLim <dpwns523@naver.com>
2022-10-03 16:19:53 +0900

    refactor: Review Controller Swagger 설명 추가

--- src/main/java/com/jjbacsa/jjbacsabackend/review/controller/ReviewController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/review/controller/ReviewController.java
@@ -6,6 +6,7 @@ import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewDeleteResponse;
 import com.jjbacsa.jjbacsabackend.review.dto.response.ReviewResponse;
 import com.jjbacsa.jjbacsabackend.review.service.ReviewService;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.Authorization;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,6 +16,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -22,66 +24,109 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "리뷰 작성",
+            notes = "리뷰를 작성합니다. MediaType은 MULTIPART_FORM_DATA_VALUE를 선택해주세요.\n\n" +
+                    "example : \n\n" +
+                    "{\n\n" +
+                    "       \"shopId\" : \"상점 id\"\n\n" +
+                    "       \"content\" : \"내용\"\n\n" +
+                    "       \"rate\" : \"별점\"\n\n" +
+                    "       \"reviewImages\" : \"리뷰 이미지\"\n\n" +
+                    "}",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @PostMapping(value = "/review", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ReviewResponse> createReview(@ModelAttribute ReviewRequest reviewRequest) throws Exception {
         return new ResponseEntity<>(reviewService.createReview(reviewRequest), HttpStatus.CREATED);
     }
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "리뷰 조회",
+            notes = "단건 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/review/{review-id}")
-    public ResponseEntity<ReviewResponse> getReview(@PathVariable("review-id") Long reviewId){
+    public ResponseEntity<ReviewResponse> getReview(@ApiParam("조회할 리뷰 id") @PathVariable("review-id") Long reviewId){
         return new ResponseEntity<>(reviewService.getReview(reviewId), HttpStatus.OK);
     }
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+
+    @ApiOperation(
+            value = "내 리뷰 조회",
+            notes = "내가 작성한 리뷰를 모두 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/review")
-    public ResponseEntity<Page<ReviewResponse>> getMyReviews(@RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
+    public ResponseEntity<Page<ReviewResponse>> getMyReviews(@ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
         return new ResponseEntity<>(reviewService.getMyReviews(page, size), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/review/search/shop/{shop-id}")
-    public ResponseEntity<Page<ReviewResponse>> searchShopReview(@PathVariable("shop-id") Long shopId, @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size){
+    // TODO: 관리자 권한으로 변경
+    @ApiOperation(
+            value = "상점에 대한 모든 리뷰 조회",
+            notes = "상점에 대한 모든 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
+    public ResponseEntity<Page<ReviewResponse>> searchShopReview(@ApiParam("조회할 상점id") @PathVariable("shop-id") Long shopId, @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size){
         return new ResponseEntity<>(reviewService.searchShopReviews(shopId, page, size), HttpStatus.OK);
     }
 
+    // TODO: 관리자 권한으로 변경
+    @ApiOperation(
+            value = "특정사용자가 작성한 모든 리뷰 조회",
+            notes = "사용자에 대한 모든 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value = "/review/search/writer/{writer-id}")
-    public ResponseEntity<Page<ReviewResponse>>searchWriterReview(@PathVariable("writer-id") Long writerId, @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size){
+    public ResponseEntity<Page<ReviewResponse>>searchWriterReview(@ApiParam("작성자 id") @PathVariable("writer-id") Long writerId, @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size){
         return new ResponseEntity<>(reviewService.searchWriterReviews(writerId, page, size), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "리뷰 삭제",
+            notes = "리뷰를 삭제합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @DeleteMapping(value = "/review")
-    public ResponseEntity<ReviewDeleteResponse>deleteReview(@RequestParam("delete-id") Long reviewId) throws Exception {
+    public ResponseEntity<ReviewDeleteResponse>deleteReview(@ApiParam("삭제할 리뷰 id") @RequestParam("delete-id") Long reviewId) throws Exception {
         return new ResponseEntity<>(reviewService.deleteReview(reviewId), HttpStatus.OK);
     }
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+
+    @ApiOperation(
+            value = "리뷰 수정",
+            notes = "리뷰를 수정합니다. MediaType은 MULTIPART_FORM_DATA_VALUE를 선택해주세요.\n\n" +
+                    "example : \n\n" +
+                    "{\n\n" +
+                    "       \"shopId\" : \"상점 id\"\n\n" +
+                    "       \"content\" : \"내용\"\n\n" +
+                    "       \"rate\" : \"별점\"\n\n" +
+                    "       \"reviewImages\" : \"리뷰 이미지\"\n\n" +
+                    "}",
+            authorizations = @Authorization(value = "Bearer + accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @PatchMapping(value="/review/{review-id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<ReviewResponse> modifyReview(@ModelAttribute @RequestBody ReviewRequest reviewRequest, @PathVariable(name = "review-id") Long reviewId) throws Exception {
+    public ResponseEntity<ReviewResponse> modifyReview(@ModelAttribute @RequestBody ReviewRequest reviewRequest, @ApiParam("수정할 리뷰 id") @PathVariable(name = "review-id") Long reviewId) throws Exception {
         return new ResponseEntity<>(reviewService.modifyReview(reviewRequest, reviewId), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로워 리뷰 조회",
+            notes = "팔로워가 작성한 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value="/review/search/follower")
-    public ResponseEntity<Page<ReviewResponse>> searchFollowerReview(@RequestParam("follower-account") String account, @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
+    public ResponseEntity<Page<ReviewResponse>> searchFollowerReview(@ApiParam("팔로워 account") @RequestParam("follower-account") String account, @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
         return new ResponseEntity<>(reviewService.searchFollowerReviews(account, page, size), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "팔로워들이 작성한 모든 리뷰 조회",
+            notes = "팔로워들이 작성한 모든 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value="/review/follower")
-    public ResponseEntity<Page<ReviewResponse>> getFollowersReviews(@RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
+    public ResponseEntity<Page<ReviewResponse>> getFollowersReviews( @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
         return new ResponseEntity<>(reviewService.getFollowersReviews(page, size), HttpStatus.OK);
     }
 
-    @Auth
-    @ApiOperation(value = "", notes = "", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @ApiOperation(
+            value = "특정 상점에 대해 팔로워들이 작성한 리뷰 조회",
+            notes = "특정 상점에 대해 팔로워들이 작성한 리뷰를 조회합니다.\n\n", authorizations = @Authorization(value = "Bearer +accessToken"))
+    @PreAuthorize("hasRole('NORMAL')")
     @GetMapping(value="/review/search/shop/{shop-id}/follower")
-    public ResponseEntity<Page<ReviewResponse>> searchFollowersShopReviews(@PathVariable("shop-id") Long shopId, @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
+    public ResponseEntity<Page<ReviewResponse>> searchFollowersShopReviews(@ApiParam("조회할 상점id") @PathVariable("shop-id") Long shopId, @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required=false, defaultValue="3")Integer size) throws Exception {
         return new ResponseEntity<>(reviewService.searchFollowersShopReviews(shopId, page, size), HttpStatus.OK);
     }
 }
