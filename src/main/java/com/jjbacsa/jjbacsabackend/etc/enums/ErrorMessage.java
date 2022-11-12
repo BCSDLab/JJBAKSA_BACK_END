dd442614587829f637dd9649ba86b98b662fddf3
yj kim <yjee0215@gmail.com>
2022-11-12 19:19:29 +0900

    refact: Error Message 21 Response code

--- src/main/java/com/jjbacsa/jjbacsabackend/etc/enums/ErrorMessage.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/etc/enums/ErrorMessage.java
@@ -24,7 +24,7 @@ public enum ErrorMessage {
     INVALID_REQUEST_EXCEPTION(18,"API 요쳥 형식이 올바르지 않습니다.",HttpStatus.BAD_REQUEST),
     OVER_QUERY_LIMIT_EXCEPTION(19,"API 요청을 수행할 수 없습니다.",HttpStatus.FORBIDDEN),
     REQUEST_DENIEDE_EXCEPTION(20,"API 요청이 거절됩니다.",HttpStatus.UNAUTHORIZED),
-    JSON_PROCESSING_EXCEPTION(21,"JSON 변환 과정에서 에러가 발생합니다.",HttpStatus.UNPROCESSABLE_ENTITY),
+    JSON_PROCESSING_EXCEPTION(21,"JSON 변환 과정에서 에러가 발생합니다.",HttpStatus.INTERNAL_SERVER_ERROR),
     REQUIRED_ATTRIBUTE_MISSING_EXCEPTION(22,"API 필수 속성이 없습니다.",HttpStatus.NOT_FOUND),
     SHOP_NOT_EXISTS_EXCEPTION(23, "상점이 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
     REVIEW_NOT_EXISTS_EXCEPTION(24, "리뷰가 존재하지 않습니다.", HttpStatus.NOT_FOUND),
