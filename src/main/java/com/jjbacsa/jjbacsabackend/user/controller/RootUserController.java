615b1b702fe90d822be091be8115e2077061a577
YeJunLim <dpwns523@naver.com>
2022-11-26 17:50:43 +0900

    feat: api 문서 추가

--- src/main/java/com/jjbacsa/jjbacsabackend/user/controller/RootUserController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/user/controller/RootUserController.java
@@ -2,6 +2,7 @@ package com.jjbacsa.jjbacsabackend.user.controller;
 
 import com.jjbacsa.jjbacsabackend.user.dto.UserResponse;
 import com.jjbacsa.jjbacsabackend.user.service.RootUserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootUserController {
     private final RootUserService rootUserService;
 
+    @ApiOperation(
+            value = "ADMIN 권한 부여",
+            notes = "사용자에게 ADMIN 권한을 부여합니다.\n\n" +
+                    "account : 유저 계정(1~20글자의 영문자 및 숫자),\n\n")
     @PostMapping(value = "/root/admin")
     public ResponseEntity<UserResponse> adminAuthority(@RequestParam String account){
         return new ResponseEntity<>(rootUserService.adminAuthority(account), HttpStatus.OK);
     }
+    @ApiOperation(
+            value = "NORMAL 권한 부여",
+            notes = "사용자에게 NORMAL 권한을 부여합니다.\n\n" +
+                    "account : 유저 계정(1~20글자의 영문자 및 숫자),\n\n")
     @PostMapping(value = "/root/normal")
     public ResponseEntity<UserResponse> normalAuthority(@RequestParam String account){
         return new ResponseEntity<>(rootUserService.normalAuthority(account), HttpStatus.OK);
