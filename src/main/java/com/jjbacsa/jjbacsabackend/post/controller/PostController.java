21c775bc2e3f967011c6fe32bf95b200f63f6aeb
YeJun <dpwns523@naver.com>
2023-02-06 17:23:23 +0900

    refactor: 페이징 및 정렬 수정, API 문서 업데이트

--- src/main/java/com/jjbacsa/jjbacsabackend/post/controller/PostController.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/post/controller/PostController.java
@@ -1,13 +1,11 @@
 package com.jjbacsa.jjbacsabackend.post.controller;
 
 import com.jjbacsa.jjbacsabackend.etc.annotations.ValidationGroups;
+import com.jjbacsa.jjbacsabackend.etc.dto.CustomPageRequest;
 import com.jjbacsa.jjbacsabackend.post.dto.request.PostRequest;
 import com.jjbacsa.jjbacsabackend.post.dto.response.PostResponse;
 import com.jjbacsa.jjbacsabackend.post.service.PostService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -15,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.Pattern;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,7 +24,10 @@ public class PostController {
 
     @ApiOperation(
             value = "Post 조회",
-            notes = "post-id : 조회할 Post의 id")
+            notes = "Post를 조회합니다.\n\n" +
+                    "{\n\n" +
+                    "       \"postId\" : \"조회할 Post의 id\"\"\n\n" +
+                    "}")
     @ApiResponses({
             @ApiResponse(code = 200,
                 message = "읽어온 Post 정보",
@@ -38,12 +41,12 @@ public class PostController {
     @ApiOperation(
             value = "FAQ, NOTICE 작성",
             notes = "Post를 작성합니다.\n\n" +
-                    "ADMIN 권한이 필요합니다.\n\n\t" +
-                    "필요한 필드\n\n\t" +
-                    "{\n\n     " +
-                    "title : 제목,\n\n     " +
-                    "content : 내용,\n\n     " +
-                    "boardType : Post 타입(FAQ, NOTICE) \n\n\t}")
+                    "ADMIN 권한이 필요합니다.\n\n" +
+                    "{\n\n" +
+                    "       \"title\" : \"제목\"\n\n" +
+                    "       \"content\" : \"내용\"\n\n"+
+                    "       \"boardType\" : \"NOTICE, POWER_NOTICE, FAQ\"\n\n"+
+                    "}", authorizations = @Authorization(value = "Bearer +accessToken"))
     @ApiResponses({
             @ApiResponse(code = 201,
                     message = "작성한 Post 정보",
@@ -51,18 +54,19 @@ public class PostController {
     })
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping(value = "/admin/post")
-    public ResponseEntity<PostResponse> create(@Validated(ValidationGroups.Create.class) @RequestBody PostRequest postRequest){
+    public ResponseEntity<PostResponse> create(@Validated(ValidationGroups.AdminCreate.class) @RequestBody PostRequest postRequest){
         return new ResponseEntity<>(postService.createPost(postRequest), HttpStatus.CREATED);
     }
 
     @ApiOperation(
             value = "FAQ, NOTICE 수정",
             notes = "Post를 수정합니다.\n\n" +
-                    "ADMIN 권한이 필요합니다.\n\n\t" +
-                    "수정할 수 있는 필드\n\n\t" +
-                    "{\n\n     " +
-                    "title : 제목,\n\n     " +
-                    "content : 내용 \n\n\t}     ")
+                    "ADMIN 권한이 필요합니다.\n\n" +
+                    "{\n\n" +
+                    "       \"postId\" : \"삭제할 Post의 id\"\"\n\n" +
+                    "       \"title\" : \"제목\"\n\n" +
+                    "       \"content\" : \"내용\"\n\n"+
+                    "}", authorizations = @Authorization(value = "Bearer +accessToken"))
     @ApiResponses({
             @ApiResponse(code = 200,
                     message = "수정한 Post 정보",
@@ -77,8 +81,9 @@ public class PostController {
     @ApiOperation(
             value = "FAQ, NOTICE 삭제",
             notes = "ADMIN 권한이 필요합니다.\n\n" +
-                    "post-id : 삭제할 Post id"
-                    )
+                    "{\n\n" +
+                    "       \"postId\" : \"삭제할 Post의 id\"\"\n\n" +
+                    "}", authorizations = @Authorization(value = "Bearer +accessToken"))
     @ApiResponses({
             @ApiResponse(code = 204,
                     message = "반환값 없음")
@@ -92,17 +97,22 @@ public class PostController {
 
     @ApiOperation(
             value = "Post 조회",
-            notes = "boardType : 조회할 Post의 id\n\n" +
-                    "page: 조회할 Post page\n\n" +
-                    "size: page의 size")
+            notes = "Post를 조회합니다.\n\n"+
+                    "example : \n\n"+
+                    "{\n\n"+
+                    "       \"boardType\" : \"BoardType은 NOTICE(공지) / FAQ, INQUERY는 문의하기페이지에 같이 게시)\"\n\n" +
+                    "       \"page\" : \"페이지 default: 0\"\n\n" +
+                    "       \"size\" : \"페이지 크기 default: 10\"\n\n" +
+                    "       \"sort\": \"정렬 기준은 작성일 기준이며, POWER_NOTICE부터, 문의하기는 FAQ부터 반환됩니다. 값을 입력하지 않으셔도 됩니다.\"\n\n" +
+                    "}", authorizations = @Authorization(value = "Bearer +accessToken"))
     @ApiResponses({
             @ApiResponse(code = 200,
                     message = "Post Page",
                     response = Page.class)
     })
     @GetMapping(value = "/post")
-    public ResponseEntity<Page<PostResponse>> getPosts(@RequestParam String boardType, @ApiParam("조회할 페이지") @RequestParam(value = "page", required = false, defaultValue = "0")Integer page, @ApiParam("페이징 사이즈") @RequestParam(value = "size", required = false, defaultValue = "3")Integer size){
-        return new ResponseEntity<>(postService.getPosts(boardType, page, size), HttpStatus.OK);
+    public ResponseEntity<Page<PostResponse>> getPosts(@RequestParam @Pattern(regexp = "^(FAQ|NOTICE|INQUIRY|POWER_NOTICE)$", message = "올바른 게시글 타입이 아닙니다.") String boardType, @Validated CustomPageRequest pageable){
+        return new ResponseEntity<>(postService.getPosts(boardType, pageable.of()), HttpStatus.OK);
     }
 
 }
