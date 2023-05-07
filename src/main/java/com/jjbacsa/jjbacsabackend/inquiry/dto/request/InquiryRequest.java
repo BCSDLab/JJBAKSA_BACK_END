e1fc093078944ca1c5eebdc2ad738f0ccfd94830
YeJun <dpwns523@naver.com>
2023-05-07 19:46:05 +0900

    feat: 이미지, 작성자 추가에 따른 수정

--- src/main/java/com/jjbacsa/jjbacsabackend/inquiry/dto/request/InquiryRequest.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/inquiry/dto/request/InquiryRequest.java
@@ -1,21 +1,28 @@
 package com.jjbacsa.jjbacsabackend.inquiry.dto.request;
 
+import com.jjbacsa.jjbacsabackend.etc.annotations.IsValidListSize;
 import com.jjbacsa.jjbacsabackend.etc.annotations.ValidationGroups;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.EntityListeners;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class InquiryRequest {
-    @NotNull(groups = {ValidationGroups.Create.class}, message = "제목을 입력해주세요")
+    @Pattern(regexp = "^[a-zA-z가-힣0-9]{1,20}$", message = "작성자에 특수문자와 초성은 불가능합니다.")
+    private String createdBy;
+    @NotNull(message = "제목을 입력해주세요")
     private String title;
-    @NotNull(groups = {ValidationGroups.Create.class}, message = "문의 내용을 입력해주세요")
+    @NotNull(message = "문의 내용을 입력해주세요")
     private String content;
-    @NotNull(groups = {ValidationGroups.Create.class}, message = "비밀글 여부를 선택해주세요")
+    @NotNull(message = "비밀글 여부를 선택해주세요")
     private Boolean isSecret;
-
+    @IsValidListSize(max = 3, message = "문의글 이미지는 최대 3개 입니다.")
+    private List<MultipartFile> inquiryImages;
 }
--- src/main/java/com/jjbacsa/jjbacsabackend/inquiry/dto/response/InquiryResponse.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/inquiry/dto/response/InquiryResponse.java
@@ -1,21 +1,25 @@
 package com.jjbacsa.jjbacsabackend.inquiry.dto.response;
 
+import com.jjbacsa.jjbacsabackend.inquiry_image.dto.response.InquiryImageResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class InquiryResponse {
+    private Long id;
     private String title;
     private String content;
     private String answer;
     private String createdBy;
     private String createdAt;
+    private List<InquiryImageResponse> inquiryImages;
     private int isSecreted;
 
     public void setCreatedAt(Date createAt){
