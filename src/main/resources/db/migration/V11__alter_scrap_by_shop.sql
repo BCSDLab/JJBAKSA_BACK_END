ea16db369f69c52c5fd1d6a9efcd1548e51ab2c9
yj kim <yjee0215@gmail.com>
2023-05-24 22:09:20 +0900

    db migration conflicts

--- src/main/resources/db/migration/V11__alter_scrap_by_shop.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE scrap DROP FOREIGN KEY scrap_ibfk_2;
-
-ALTER TABLE scrap ADD CONSTRAINT scrap_google_shop_fk FOREIGN KEY (shop_id) REFERENCES google_shop (id) ON DELETE SET NULL;
--- src/main/resources/db/migration/V12__create_withdraw_reason_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS withdraw_reason
-(
-    user_id          BIGINT       PRIMARY KEY,
-    reason           VARCHAR(30)  NOT NULL,
-    discomfort       VARCHAR(255) NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE
-) default character set utf8mb4
-collate utf8mb4_bin;
