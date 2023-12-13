0619fde10bccd7de013af099d932655a296c7563
yj kim <yjee0215@gmail.com>
2023-12-14 07:57:06 +0900

    refeat: 검색 서비스에서 Redis Key 주입 방식으로 변경
    - 테스트에 용이하기 위해 변경하도록 함

--- src/main/java/com/jjbacsa/jjbacsabackend/search/serviceImpl/SearchServiceImpl.java
+++ src/main/java/com/jjbacsa/jjbacsabackend/search/serviceImpl/SearchServiceImpl.java
@@ -2,43 +2,48 @@ package com.jjbacsa.jjbacsabackend.search.serviceImpl;
 
 import com.jjbacsa.jjbacsabackend.search.dto.TrendingResponse;
 import com.jjbacsa.jjbacsabackend.search.service.SearchService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
 public class SearchServiceImpl implements SearchService {
 
     private final StringRedisTemplate redisTemplate;
-    private final String RANKING = "RANKING";
+    private final String ranking;
+
+    public SearchServiceImpl(StringRedisTemplate redisTemplate, @Value("${spring.redis.ranking}") String ranking) {
+        this.redisTemplate = redisTemplate;
+        this.ranking = ranking;
+
+    }
 
     @Override
     public TrendingResponse getTrending() {
         return TrendingResponse.builder().
-                trendings(redisTemplate.opsForZSet().reverseRange(RANKING, 0, -1).stream().collect(Collectors.toList()))
+                trendings(redisTemplate.opsForZSet().reverseRange(ranking, 0, -1).stream().collect(Collectors.toList()))
                 .build();
     }
 
     @Override
     public void saveTrending(String keyword) {
-        List<String> rankingList = redisTemplate.opsForZSet().reverseRange(RANKING, 0, -1).stream().collect(Collectors.toList());
+        List<String> rankingList = redisTemplate.opsForZSet().reverseRange(ranking, 0, -1).stream().collect(Collectors.toList());
 
-        redisTemplate.opsForZSet().incrementScore(RANKING, keyword, 2);
+        redisTemplate.opsForZSet().incrementScore(ranking, keyword, 2);
 
         for (String ranking : rankingList) {
             if (!ranking.equals(keyword)) {
-                redisTemplate.opsForZSet().incrementScore(RANKING, ranking, -1);
+                redisTemplate.opsForZSet().incrementScore(this.ranking, ranking, -1);
             }
         }
 
-        long size = redisTemplate.opsForZSet().zCard(RANKING);
+        long size = redisTemplate.opsForZSet().zCard(ranking);
         if (size > 10) {
             long offset = size - 10;
-            redisTemplate.opsForZSet().removeRange(RANKING, 0, offset - 1);
+            redisTemplate.opsForZSet().removeRange(ranking, 0, offset - 1);
         }
     }
 }
